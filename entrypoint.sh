ea97b8502d952a40b2965b51f7da25bac448271b
Seungmin Kim <a7965344@gmail.com>
2025-04-14 08:45:20 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,12 @@ NETWORK_ID=${NETWORK_ID:-250407}
 exec /usr/local/bin/worldland \
     --networkid "$NETWORK_ID" \
     --datadir /workspace/BCAInetwork \
-    -port 30303 \
+    --port 30303 \
+    --addr 0.0.0.0 \ 
     --http \
-    --http.vhosts "*" \
+    --http.addr 0.0.0.0 \
     --http.corsdomain "*" \
+    --http.vhosts "*" \
     --http.api "eth,net,web3" \
     -nodiscover \
     -verbosity 3 \
